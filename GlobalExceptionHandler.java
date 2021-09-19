10153aa3d9f60b49190430962dff1b86dbdd37a4
mohd_irfan <39884239+irfanmohd@users.noreply.github.com>
2021-09-19 17:39:47 +0530

    Add files via upload

---
+++ GlobalExceptionHandler.java
@@ -0,0 +1,53 @@
+package com.nagaro.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+
+	private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@ExceptionHandler(AccountStatementNotFoundException.class)
+	public ResponseEntity<ExceptionResponse> exception(final AccountStatementNotFoundException exception,
+			final HttpServletRequest request) {
+		logger.error("Account  Statement Not Found Exception: ", exception.getMessage());
+		
+		ExceptionResponse error = new ExceptionResponse();
+		error.setErrorMessage(exception.getMessage());
+		error.callerURL(request.getRequestURI());
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(InvalidInputException.class)
+	public ResponseEntity<Object> handleInvalidInputException(InvalidInputException exception,
+			final HttpServletRequest request) {
+		logger.error("Invalid Input Exception: ", exception.getMessage());
+		
+		ExceptionResponse error = new ExceptionResponse();
+		error.setErrorMessage(exception.getMessage());
+		error.callerURL(request.getRequestURI());
+		return new ResponseEntity<>(error,  HttpStatus.NOT_FOUND);
+	}
+
+	
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleInternalServerException(Exception exception,
+			final HttpServletRequest request) {
+		logger.error("Internal Server Exception: ", exception.getMessage());
+		
+		ExceptionResponse error = new ExceptionResponse();
+		error.setErrorMessage(exception.getMessage());
+		error.callerURL(request.getRequestURI());
+		return new ResponseEntity<>(error,  HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
